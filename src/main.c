e5f2ca5a680199dc0679fa24089e47daa2f87aff
Ilia Riabsev <46lidiay@gmail.com>
2024-11-26 11:00:42 +0200

    Added Remove brush

--- src/main.c
+++ src/main.c
@@ -4,8 +4,8 @@
 #include <time.h>
 #include <string.h>
 
-#define GRID_SIZE 500    
-#define CELL_SIZE 3      
+#define GRID_SIZE 500
+#define CELL_SIZE 3
 #define SCREEN_WIDTH 800
 #define SCREEN_HEIGHT 600
 
@@ -25,31 +25,31 @@ void DrawStats(int iteration, int population);
 void AddBrushWithMouse(GameOfLife* game, Vector2 mousePosition, int brushSize);
 
 int main(void) {
-    
-    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game of Life");
-    SetTargetFPS(60); 
 
-    
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Game of Life");
+    SetTargetFPS(60);
+
+
     GameOfLife game;
     InitGameOfLife(&game, GRID_SIZE, GRID_SIZE);
 
     int iteration = 0;
     int population = 0;
-    int speed = 60;  
-    bool isPaused = false;  
+    int speed = 60;
+    bool isPaused = false;
 
-    char speedText[10] = "10"; 
-    bool isSettingSpeed = false;  
+    char speedText[10] = "10";
+    bool isSettingSpeed = false;
 
-    
+
     float timeAccumulator = 0.0f;
-    float updateInterval = 1.0f / speed;  
+    float updateInterval = 1.0f / speed;
 
     int brushSize = 0;
-    
+
     srand(time(NULL));
-    for (int y = 0; y < game.height; y++) {
-        for (int x = 0; x < game.width; x++) {
+    for (int y = 0; y < SCREEN_HEIGHT / CELL_SIZE; y++) {
+        for (int x = 0; x < SCREEN_WIDTH / CELL_SIZE; x++) {
             if (rand() % 2) {
                 SetCell(&game, x, y, true);
                 population++;
@@ -57,37 +57,38 @@ int main(void) {
         }
     }
 
-    
+
+
     while (!WindowShouldClose()) {
-        
+
         float deltaTime = GetFrameTime();
         timeAccumulator += deltaTime;
 
-        
+
         if (IsKeyDown(KEY_UP)) {
             speed = (speed < 60) ? speed + 1 : 60;
             updateInterval = 1.0f / speed;
         }
         if (IsKeyDown(KEY_DOWN)) {
-            speed = (speed > 1) ? speed - 1 : 1;  
+            speed = (speed > 1) ? speed - 1 : 1;
             updateInterval = 1.0f / speed;
         }
         if (IsKeyPressed(KEY_SPACE)) {
-            isPaused = !isPaused;  
+            isPaused = !isPaused;
         }
         if (IsKeyPressed(KEY_R)) {
-            speed = 1;  
+            speed = 1;
             updateInterval = 1.0f / speed;
         }
         if (IsKeyPressed(KEY_M)) {
-            speed = 60;  
+            speed = 60;
             updateInterval = 1.0f / speed;
         }
         if (GetMouseWheelMove() > 0) {
-            brushSize = (brushSize < 50) ? brushSize + 1 : 50;  
+            brushSize = (brushSize < 50) ? brushSize + 1 : 50;
         }
         if (GetMouseWheelMove() < 0) {
-            brushSize = (brushSize > 0) ? brushSize - 1 : 0;  
+            brushSize = (brushSize > 0) ? brushSize - 1 : 0;
         }
 
 
@@ -95,14 +96,14 @@ int main(void) {
             if (timeAccumulator >= updateInterval) {
                 UpdateGameOfLife(&game);
                 timeAccumulator = 0.0f;
-                iteration++;  
+                iteration++;
             }
         }
 
 
         population = 0;
 
-        
+
         for (int y = 0; y < game.height; y++) {
             for (int x = 0; x < game.width; x++) {
                 if (game.grid[y * game.width + x]) {
@@ -111,21 +112,21 @@ int main(void) {
             }
         }
 
-        
+
         Vector2 mousePosition = GetMousePosition();
-        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {  
+        if (IsMouseButtonDown(MOUSE_BUTTON_LEFT) || IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
             AddBrushWithMouse(&game, mousePosition, brushSize);
         }
 
-        
-        BeginDrawing();
-        ClearBackground(BLACK);  
 
-        DrawGameOfLife(&game);  
-        DrawStats(iteration, population);  
+        BeginDrawing();
+        ClearBackground(BLACK);
+
+        DrawGameOfLife(&game);
+        DrawStats(iteration, population);
 
         if (isPaused) {
-            DrawText("PAUSED", 10, 10, 30, YELLOW);  
+            DrawText("PAUSED", 10, 10, 30, YELLOW);
         }
 
         EndDrawing();
@@ -133,9 +134,9 @@ int main(void) {
 
     }
 
-    
+
     free(game.grid);
-    CloseWindow(); 
+    CloseWindow();
 
     return 0;
 }
@@ -143,7 +144,7 @@ int main(void) {
 void InitGameOfLife(GameOfLife* game, int width, int height) {
     game->width = width;
     game->height = height;
-    game->grid = (bool*)calloc(width * height, sizeof(bool));  
+    game->grid = (bool*)calloc(width * height, sizeof(bool));
 }
 
 void UpdateGameOfLife(GameOfLife* game) {
@@ -154,16 +155,16 @@ void UpdateGameOfLife(GameOfLife* game) {
             int neighbors = CountNeighbors(game, x, y);
             bool currentState = game->grid[y * game->width + x];
 
-            
+
             if (currentState && (neighbors == 2 || neighbors == 3)) {
-                newGrid[y * game->width + x] = true; 
+                newGrid[y * game->width + x] = true;
             } else if (!currentState && neighbors == 3) {
-                newGrid[y * game->width + x] = true; 
+                newGrid[y * game->width + x] = true;
             }
         }
     }
 
-    
+
     free(game->grid);
     game->grid = newGrid;
 }
@@ -181,26 +182,31 @@ void DrawGameOfLife(GameOfLife* game) {
 
 int CountNeighbors(GameOfLife* game, int x, int y) {
     int neighbors = 0;
+
     for (int j = -1; j <= 1; j++) {
         for (int i = -1; i <= 1; i++) {
             if (i == 0 && j == 0) continue;
 
-            int nx = (x + i + game->width) % game->width;
-            int ny = (y + j + game->height) % game->height;
+            int nx = x + i;
+            int ny = y + j;
 
-            if (game->grid[ny * game->width + nx]) {
-                neighbors++;
+            if (nx >= 0 && nx < game->width && ny >= 0 && ny < game->height) {
+                if (game->grid[ny * game->width + nx]) {
+                    neighbors++;
+                }
             }
         }
     }
     return neighbors;
 }
 
+
 void SetCell(GameOfLife* game, int x, int y, bool state) {
     if (x >= 0 && x < game->width && y >= 0 && y < game->height) {
         game->grid[y * game->width + x] = state;
     }
 }
+
 
 void DrawStats(int iteration, int population) {
     char stats[128];
@@ -214,14 +220,20 @@ void AddBrushWithMouse(GameOfLife* game, Vector2 mousePosition, int brushSize) {
 
     for (int y = -brushSize; y <= brushSize; y++) {
         for (int x = -brushSize; x <= brushSize; x++) {
-            if (brushSize == 0) {  
-                SetCell(game, xCenter, yCenter, true);  
-            } else {  
-                for (int y = -brushSize; y <= brushSize; y++) {  
-                    for (int x = -brushSize; x <= brushSize; x++) {  
-                        SetCell(game, xCenter + x, yCenter + y, true);  
-                    }  
-                }  
+            if (brushSize == 0) {
+                if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                    SetCell(game, xCenter, yCenter, true);
+                }
+                if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+                    SetCell(game, xCenter, yCenter, false);
+                }
+            } else {
+                if (IsMouseButtonDown(MOUSE_BUTTON_LEFT)) {
+                    SetCell(game, xCenter + x, yCenter + y, true);
+                }
+                if (IsMouseButtonDown(MOUSE_BUTTON_RIGHT)) {
+                    SetCell(game, xCenter + x, yCenter + y, false);
+                }
             }
         }
     }
